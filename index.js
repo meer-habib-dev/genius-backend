79422c65ae3d10ca59d8df5ddbaeedc896a48837
meer-habib-dev <contact.freelancermir@gmail.com>
2021-10-27 01:13:36 +0600

    heroku testing

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Running genius server");
+});
+
 app.listen(port, () => {
   console.log("Listening to port of:", port);
 });
